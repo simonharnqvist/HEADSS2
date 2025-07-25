d08139d1a9e6e36388f8977656fd6fb1e2ceb106
simonharnqvist <sharnqvi@roe.ac.uk>
2025-07-25 11:57:07 +0100

    cleaner API

---
+++ headss2/__init__.py
@@ -0,0 +1,7 @@
+from headss2.datasets import dataset
+from headss2.regions import make_regions
+from headss2.clustering import cluster
+from headss2.stitching import stitch
+from headss2.merging import merge_clusters
+
+__all__ = ["dataset", "make_regions", "cluster", "stitch", "merge_clusters"]
