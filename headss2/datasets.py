2d942573c12c52d329dcec09d709f60ceee92a4a
simonharnqvist <sharnqvi@roe.ac.uk>
2025-08-25 12:05:24 +0100

    moved example data to correct location

--- headss2/datasets.py
+++ headss2/datasets.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 def dataset(name: str) -> pd.DataFrame:
     """Retrieve example dataset by name. Current datasets are 'a3', 'Aggregation', 'flame', 'pathbased', 'spiral', 'D31', 'birch1', 'jain', t4_8k', 'worms'"""
-    script_path = Path(__file__).resolve().parent.parent
+    script_path = Path(__file__).resolve().parent
     path = script_path.joinpath("example_data").joinpath(f"{name}.csv")
     return pd.read_csv(path, header=None).iloc[:, 0:2].rename(columns = {0:'x', 1:'y'})
