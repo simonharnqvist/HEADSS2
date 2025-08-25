0209118f3d9e46ee470207cc6afe36332b0a155f
simonharnqvist <sharnqvi@roe.ac.uk>
2025-08-25 12:14:00 +0100

    fixed data imports

--- headss2/datasets.py
+++ headss2/datasets.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from pathlib import Path
+import importlib.resources as pkg_resources
+from headss2 import example_data
 
 def dataset(name: str) -> pd.DataFrame:
-    """Retrieve example dataset by name. Current datasets are 'a3', 'Aggregation', 'flame', 'pathbased', 'spiral', 'D31', 'birch1', 'jain', t4_8k', 'worms'"""
-    script_path = Path(__file__).resolve().parent
-    path = script_path.joinpath("example_data").joinpath(f"{name}.csv")
-    return pd.read_csv(path, header=None).iloc[:, 0:2].rename(columns = {0:'x', 1:'y'})
+    filename = f"{name}.csv"
+    with pkg_resources.open_text(example_data, filename) as f:
+        df = pd.read_csv(f, header=None)
+    return df.iloc[:, 0:2].rename(columns={0: 'x', 1: 'y'})
